fd51c43410c34933e33618d5050bb7855d49edef
OmerFS <omerfaruksoylemez90@gmail.com>
2023-06-24 17:50:48 +0300

    2 ders sonu

--- src/test/java/practice/P02.java
+++ src/test/java/practice/P02.java
@@ -1,4 +1,29 @@
 package practice;
 
+import org.junit.*;
+
 public class P02 {
+
+    @BeforeClass
+    public static void beforeClass(){System.out.println("Before Class çalıştı");}
+
+    @AfterClass
+    public static void afterClass(){System.out.println("After Class çalıştı");}
+
+    @Before
+    public void before() {System.out.println("Yeni bir test çalışıyor");}
+
+    @After
+    public void after() {System.out.println("Test sonlandırıldı sıradaki testlere geçiliyor");}
+
+    @Test
+    public void test01 () {
+        System.out.println("1 Numaralı test çalışıyor");
+    }
+
+    @Test
+    public void test02 () {
+        System.out.println("2 Numaralı test çalışıyor");
+    }
+
 }
