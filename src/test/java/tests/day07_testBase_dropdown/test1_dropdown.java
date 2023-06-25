0bce5f31c12e8e7c24960c582b0a1180566b6771
OmerFS <omerfaruksoylemez90@gmail.com>
2023-06-25 13:30:33 +0300

    2 ders sonu

--- src/test/java/tests/day07_testBase_dropdown/test1_dropdown.java
+++ src/test/java/tests/day07_testBase_dropdown/test1_dropdown.java
@@ -1,6 +1,14 @@
 package tests.day07_testBase_dropdown;
 
-public class test1_dropdown {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.TestBaseQuit;
+
+public class test1_dropdown extends TestBaseQuit {
 
         // ● Bir class oluşturun: C3_DropDownAmazon
         // ● https://www.amazon.com/ adresine gidin.
@@ -12,4 +20,49 @@ public class test1_dropdown {
         // 2. Arama kutusuna Java yazin ve aratin
         // 3. Bulunan sonuc sayisini yazdirin
         // 4. Sonucun Java kelimesini icerdigini test edin
+
+    @Test
+    public void Test01 () {
+
+        // ● https://www.amazon.com/ adresine gidin.
+        driver.get("https://www.amazon.com/");
+
+        //             - Test 1
+
+        // Arama kutusunun yanindaki kategori menusundeki kategori sayisinin 28
+        // oldugunu test edin
+        bekle(3);
+        WebElement dropDownMenuElementi = driver.findElement(By.xpath("//select[@id='searchDropdownBox']"));
+        Select select = new Select(dropDownMenuElementi);
+        int expectedSayi = 28;
+        int actualSayi = select.getOptions().size();
+
+        Assert.assertEquals(expectedSayi,actualSayi);
+        bekle(2);
+
+        //              -Test 2
+        // 1. Kategori menusunden Books secenegini secin
+        bekle(2);
+        select.selectByVisibleText("Books");
+        bekle(5);
+
+
+        // 2. Arama kutusuna Java yazin ve aratin
+        bekle(5);
+        WebElement aramakutusu = driver.findElement(By.xpath("//*[@id='twotabsearchtextbox']"));
+        aramakutusu.sendKeys("Java" + Keys.ENTER);
+        bekle(5);
+
+
+        // 3. Bulunan sonuc sayisini yazdirin
+        WebElement sonucSayisi = driver.findElement(By.className("sg-col-inner"));
+        System.out.println("Bulunan sonuç sayisi: " + sonucSayisi.getText());
+
+
+
+        // 4. Sonucun Java kelimesini icerdigini test edin
+        Assert.assertTrue(sonucSayisi.getText().contains("Java"));
+
+    }
 }
+
--- src/test/java/tests/day07_testBase_dropdown/test2_dropdown.java
+++ src/test/java/tests/day07_testBase_dropdown/test2_dropdown.java
@@ -1,6 +1,12 @@
 package tests.day07_testBase_dropdown;
 
-public class test2_dropdown {
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.TestBaseQuit;
+
+public class test2_dropdown extends TestBaseQuit {
 
     // 1. http://zero.webappsecurity.com/ Adresine gidin
     // 2. Sign in butonuna basin
@@ -16,4 +22,53 @@ public class test2_dropdown {
     // 12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
     // 13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini
     //      kontrol edin.
+
+    @Test
+    public void Test01 () {
+
+        // 1. http://zero.webappsecurity.com/ Adresine gidin
+        driver.get("http://zero.webappsecurity.com/");
+
+
+        // 2. Sign in butonuna basin
+        bekle(2);
+        driver.findElement(By.xpath("//button[@id='signin_button']")).click();
+        bekle(2);
+
+
+        // 3. Login kutusuna “username” yazin
+        bekle(2);
+        WebElement userName = driver.findElement(By.xpath("//input[@type='text']"));
+        userName.sendKeys("username");
+        bekle(2);
+
+
+        // 4. Password kutusuna “password.” yazin
+        bekle(2);
+        WebElement passWord = driver.findElement(By.xpath("//input[@type='password']"));
+        passWord.sendKeys("password");
+
+
+        // 5. Sign in tusuna basin
+        bekle(2);
+        driver.findElement(By.xpath("//input[@class='btn btn-primary']")).click();
+        bekle(4);
+
+
+        // SORUDA SORUN VAR...
+
+        // 6. Pay Bills sayfasina gidin
+        // 7. “Purchase Foreign Currency” tusuna basin
+        // 8. “Currency” drop down menusunden Eurozone’u secin
+        // 9. “amount” kutusuna bir sayi girin
+        // 10. “US Dollars” in secilmedigini test edin
+        // 11. “Selected currency” butonunu secin
+        // 12. “Calculate Costs” butonuna basin sonra “purchase” butonuna basin
+        // 13. “Foreign currency cash was successfully purchased.” yazisinin ciktigini
+        //      kontrol edin.
+
+
+
+
+    }
 }
