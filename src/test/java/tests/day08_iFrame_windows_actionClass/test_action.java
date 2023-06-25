ff272b52742d24532465d0d1ad1cb764679861eb
OmerFS <omerfaruksoylemez90@gmail.com>
2023-06-25 10:22:47 +0300

    2 ders sonu

--- src/test/java/tests/day08_iFrame_windows_actionClass/test_action.java
+++ src/test/java/tests/day08_iFrame_windows_actionClass/test_action.java
@@ -1,6 +1,15 @@
 package tests.day08_iFrame_windows_actionClass;
 
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import utilities.TestBase;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.beans.PropertyChangeListener;
 
 public class test_action extends TestBase {
 
@@ -10,5 +19,36 @@ public class test_action extends TestBase {
         //    mouse’u bu menunun ustune getirin
         // 3- “Create a list” butonuna basin
         // 4- Acilan sayfada “Your Lists” yazisi oldugunu test edin
+
+    @Test
+    public void Test01 () {
+
+        // 1- https://www.amazon.com/ adresine gidin
+        driver.get("https://www.amazon.com/");
+        bekle(2);
+
+        // 2- Sag ust bolumde bulunan “Account & Lists” menusunun acilmasi icin
+        //    mouse’u bu menunun ustune getirin
+        bekle(2);
+        Actions actions = new Actions(driver);
+        WebElement element = driver.findElement(By.xpath("(//a[@data-nav-role='signin'])[1]"));
+        actions.moveToElement(element).perform();
+        bekle(2);
+
+
+        // 3- “Create a list” butonuna basin
+        driver.findElement(By.xpath("//span[text()='Create a List']")).click();
+        bekle(2);
+
+
+        // 4- Acilan sayfada “Your Lists” yazisi oldugunu test edin
+        WebElement YourList = driver.findElement(By.xpath("//div[@role='heading']"));
+
+        String expectedYazi = "Your Lists";
+        String actualYazi = YourList.getText();
+
+        Assert.assertTrue(actualYazi.contains(expectedYazi));
+
+    }
 
 }
--- src/test/java/tests/day08_iFrame_windows_actionClass/test_handleWindows.java
+++ src/test/java/tests/day08_iFrame_windows_actionClass/test_handleWindows.java
@@ -1,6 +1,12 @@
 package tests.day08_iFrame_windows_actionClass;
 
-public class test_handleWindows {
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.WindowType;
+import utilities.TestBaseQuit;
+
+public class test_handleWindows extends TestBaseQuit {
 
     // ● Yeni bir class olusturun: WindowHandle
     // ● Amazon anasayfa adresine gidin.
@@ -11,4 +17,53 @@ public class test_handleWindows {
     // ● Yeni bir window olusturup, acilan sayfada walmart.com adresine gidin
     // ● Sayfa title’nin “Walmart” icerdigini test edin
     // ● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+
+    @Test
+    public void Test01 () {
+
+        // ● Amazon anasayfa adresine gidin.
+        driver.get("https://www.amazon.com");
+
+
+        // ● Sayfa’nin window handle degerini String bir degiskene atayin
+        String amazonwHD = driver.getWindowHandle();
+
+
+        // ● Sayfa title’nin “Amazon” icerdigini test edin
+        String expectedtitle = "Amazon";
+        String actualtitle = driver.getTitle();
+        Assert.assertTrue(actualtitle.contains(expectedtitle));
+
+
+        // ● Yeni bir tab olusturup, acilan tab’da wisequarter.com adresine gidin
+        driver.switchTo().newWindow(WindowType.TAB).get("https://www.wisequarter.com");
+
+
+        // ● Sayfa title’nin “wisequarter” icerdigini test edin
+        expectedtitle  ="wisequarter";
+        actualtitle= driver.getTitle();
+        Assert.assertTrue(actualtitle.contains(expectedtitle));
+        bekle(3);
+
+
+        // ● Yeni bir window olusturup, acilan sayfada walmart.com adresine gidin
+        driver.switchTo().newWindow(WindowType.WINDOW).get("https://www.walmart.com");
+
+
+        // ● Sayfa title’nin “Walmart” icerdigini test edin
+        expectedtitle  ="Walmart";
+        actualtitle= driver.getTitle();
+        Assert.assertTrue(actualtitle.contains(expectedtitle));
+        bekle(3);
+
+
+        // ● Ilk acilan sayfaya donun ve amazon sayfasina dondugunuzu test edin
+        driver.switchTo().window(amazonwHD);
+        String expectedUrl = "https://www.amazon.com";
+        String actualUrl = driver.getCurrentUrl();
+
+        Assert.assertEquals(expectedUrl,actualUrl);
+
+
+    }
 }
